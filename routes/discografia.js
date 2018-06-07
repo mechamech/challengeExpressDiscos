303f08ff236b3f7a6948eed735da4b0322058f8d
Debo Reyes <reyesdebora@hotmail.com.ar>
2018-06-07 13:11:45 -0300

    login

--- routes/discografia.js
+++ routes/discografia.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/registro', function(req, res, next) {
+    res.render('registro');
+});
+
+router.get('/nuevoUsuario', function(req, res, next) {
+    res.render('nuevoUsuario');
+});
 
 router.post('/nuevo', function(req, res, next) {
     usuarios.push({nombre: req.body.nombre,email: req.body.email,password:req.body.password});
-    res.redirect('/');
-    console.log(usuarios)
+    res.redirect('');
 });
 
 router.post('/login', function(req, res, next) {
     var usuario= usuarios.find((usuario)=> {return usuario.email == req.body.email && usuario.password==req.body.password})
     if(usuario){
-        res.redirect('/');
+        res.redirect('');
     }else{
         res.send("")
     }
 });
 
-router.get('/registro', function(req, res, next) {
-    res.render('registro');
-});
+
+
+router
 
 module.exports = router;
